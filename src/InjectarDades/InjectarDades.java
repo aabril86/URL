96de006f519cf7b8ecdbd26d5fbcc1d5f11a20cd
abril <aabril@elpuig.xeill.net>
2021-03-19 18:33:34 +0100

    Initial commit

---
+++ src/InjectarDades/InjectarDades.java
@@ -0,0 +1,14 @@
+package InjectarDades;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class InjectarDades {
+    public static void main(String[] args) throws MalformedURLException {
+
+        URL url = new URL("https://docs.google.com/forms/d/e/1FAIpQLScE6sxLFb3BmCmT2TKHQH5ormS0qvjHwO-uTAR8MXaagBvSSQ/viewform?hr_submission=ChkI6uy3370CEhAI_aS577wDEgcI2aS577wDEAA");
+
+
+    }
+}
